07b3289bffb45c66bbdb80144a813b9e871e8e95
Anjana Narasinghe <nmasnanjana.123@gmail.com>
2025-03-07 22:46:48 +0530

    create: add the database connection strings

---
+++ src/config/db_config.ts
@@ -0,0 +1,16 @@
+import {Sequelize} from "sequelize";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    database: process.env.DB_NAME || 'erp',
+    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.DB_USERNAME || 'dev',
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    logging: false,
+});
+
+export default sequelize;
