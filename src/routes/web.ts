67f6a174ad7ffc64907d6c6049d39a26b21fd63c
Anjana Narasinghe <nmasnanjana.123@gmail.com>
2025-04-27 18:29:55 +0530

    update the full route middleware

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import userRoute from "./userRoute";
+import teamRoute from "./teamRoute";
+import teamAssignmentRoute from "./teamAssignmentRoute";
 
 const router = express.Router();
 
 router.use("/users", userRoute);
+router.use("/teams", teamRoute);
+router.use("/team-assignments", teamAssignmentRoute);
 
 export default router;
