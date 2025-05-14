a435b39936e44ed2d32c72bf5601390bd47d3524
Anjana Narasinghe <nmasnanjana.123@gmail.com>
2025-05-14 11:48:57 +0530

    update the associations with new associations

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -7,30 +7,37 @@ import Expense from "./expense";
 import ExpenseType from "./expenseType";
 import PurchaseOrder from "./purchaseOrder";
 import QcComment from "./qcComment";
+import OperationType from "./operationType";
 
 // Core Team-User relationships
 User.belongsToMany(Team, { through: TeamAssignment, foreignKey: 'user_id', otherKey: 'team_id', as: 'teams' });
 Team.belongsToMany(User, { through: TeamAssignment, foreignKey: 'team_id', otherKey: 'user_id', as: 'members' });
-
-// Team leadership
 Team.belongsTo(User, { foreignKey: 'leader_id', as: 'leader' });
+User.hasMany(Team, { foreignKey: 'leader_id', as: 'ledTeams' });
 
 // Job relationships
 Job.belongsTo(Team, { foreignKey: 'team_id', as: 'team' });
 Job.belongsTo(Customer, { foreignKey: 'customer_id', as: 'customer' });
+Team.hasMany(Job, { foreignKey: 'team_id', as: 'jobs' });
+Customer.hasMany(Job, { foreignKey: 'customer_id', as: 'jobs' });
 
 // Expense relationships
 Expense.belongsTo(ExpenseType, { foreignKey: 'expenses_type_id', as: 'expenseType' });
 Expense.belongsTo(Job, { foreignKey: 'job_id', as: 'job' });
+Expense.belongsTo(OperationType, { foreignKey: 'operation_type_id', as: 'operationType' });
 Expense.belongsTo(User, { foreignKey: 'edited_by', as: 'editor' });
 
 // Purchase Order relationships
 PurchaseOrder.belongsTo(Job, { foreignKey: 'job_id', as: 'job' });
 PurchaseOrder.belongsTo(User, { foreignKey: 'created_by', as: 'creator' });
+Job.hasMany(PurchaseOrder, { foreignKey: 'job_id', as: 'purchaseOrders' });
+User.hasMany(PurchaseOrder, { foreignKey: 'created_by', as: 'createdPurchaseOrders' });
 
 // QC Comment relationships
 QcComment.belongsTo(Job, { foreignKey: 'job_id', as: 'job' });
 QcComment.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+Job.hasMany(QcComment, { foreignKey: 'job_id', as: 'qcComments' });
+User.hasMany(QcComment, { foreignKey: 'user_id', as: 'qcComments' });
 
 // Export setup in case needed
 export const setupAssociations = () => {};
